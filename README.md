d932b4d3bdf421d7e535bd0e05678002ab4cccc5
SilviuManea <silviu.manea.hamzau@outlook.com>
2021-01-05 17:25:24 +0100

    lesson-10

--- README.md
+++ README.md
@@ -17,3 +17,5 @@ Lesson-7 - Creating a Component
 Lesson-8 - Nesting Components(tutorial outdated - no need to nest that way anymore, angular already does it for us.)
 
 Lesson-9 - Ng-Content Directive
+
+Lesson-10 - Data Binding (with string interpolation {{variable}} )
--- ninja-directory/src/app/home/home.component.ts
+++ ninja-directory/src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  homeTitle = "Welcome to the ninja directory";//data we can pass to the view {{string interpolation}}
   constructor() { }
 
   ngOnInit(): void {
